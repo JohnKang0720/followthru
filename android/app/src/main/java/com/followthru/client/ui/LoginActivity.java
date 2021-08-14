1dff22b036d6ae137ed549f552f91e6fd12c73e7
KevDev0247 <donkevin1980s@gmail.com>
2021-08-14 03:26:16 -0400

    Create UI components and add preliminary UI script

---
+++ android/app/src/main/java/com/followthru/client/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.followthru.client.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.followthru.client.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- android/app/src/main/java/com/followthru/client/ui/RegisterActivity.java
+++ android/app/src/main/java/com/followthru/client/ui/RegisterActivity.java
@@ -1,14 +1,16 @@
-package com.followthru.client;
+package com.followthru.client.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.followthru.client.R;
+
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 }
